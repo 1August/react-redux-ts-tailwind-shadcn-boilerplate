c8134cd66e07a3a952d864c74493adb346d76965
Maksat <boktaban@gmail.com>
2023-08-25 22:40:14 +0600

    add todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import './App.css';
 import { AppNavigation } from './navigation';
 
+/*
+  TODO:
+    - add animation library: animate.css / framer-motion
+    - create layouts
+    - jwt token realize
+    - create login, register pages
+    - zod models
+    - fix store and api requests
+
+    - ai / ml libraries
+    - react hook form check
+ */
 function App() {
   return (
     <AppNavigation/>
